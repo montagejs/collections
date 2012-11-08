aedcc930cc2d0682088410e8e49c5d275dcdcf51
Kris Kowal <kris.kowal@cixar.com>
2012-11-08 11:33:50 -0800

    Add display of observed object properties states'
    To aid in debugging with visual inspectors.  Property change listeners
    obscure a objects' state with getters and setters.  This change adds a
    __state__ object and maintains its values to reflect the most recently
    set value for each observed property, unless the __state__ property is
    not extensible.

--- listen/property-changes.js
+++ listen/property-changes.js
@@ -60,6 +60,8 @@ function PropertyChanges() {
     throw new Error("This is an abstract interface. Mix it. Don't construct it");
 }
 
+PropertyChanges.debug = true;
+
 PropertyChanges.prototype.getOwnPropertyChangeDescriptor = function (key) {
     if (!propertyChangeDescriptors.has(this)) {
         propertyChangeDescriptors.set(this, {});
@@ -177,6 +179,21 @@ PropertyChanges.prototype.makePropertyObservable = function (key) {
         return;
     }
 
+    var state;
+    if (typeof this.__state__ === "object") {
+        state = this.__state__;
+    } else {
+        state = {};
+        if (Object.isExtensible(this, "__state__")) {
+            Object.defineProperty(this, "__state__", {
+                value: state,
+                writable: true,
+                enumerable: true
+            });
+        }
+    }
+    state[key] = this[key];
+
     // memoize overridden property descriptor table
     if (!overriddenObjectDescriptors.has(this)) {
         overriddenPropertyDescriptors = {};
@@ -247,6 +264,7 @@ PropertyChanges.prototype.makePropertyObservable = function (key) {
                 }
                 PropertyChanges.dispatchBeforeOwnPropertyChange(this, key, overriddenDescriptor.value);
                 overriddenDescriptor.value = value;
+                state[key] = value;
                 PropertyChanges.dispatchOwnPropertyChange(this, key, value);
                 return value;
             },
@@ -279,6 +297,7 @@ PropertyChanges.prototype.makePropertyObservable = function (key) {
                 // was successful
                 if (overriddenDescriptor.get) {
                     value = overriddenDescriptor.get.apply(this, arguments);
+                    state[key] = value;
                 }
                 // dispatch the new value: the given value if there is
                 // no getter, or the actual value if there is one
@@ -313,6 +332,21 @@ PropertyChanges.prototype.makePropertyUnobservable = function (key) {
 
     var overriddenDescriptor = overriddenPropertyDescriptors[key];
     delete overriddenPropertyDescriptors[key];
+
+    var state;
+    if (typeof this.__state__ === "object") {
+        state = this.__state__;
+    } else {
+        state = {};
+        if (Object.isExtensible(this, "__state__")) {
+            Object.defineProperty(this, "__state__", {
+                value: state,
+                writable: true,
+                enumerable: false
+            });
+        }
+    }
+    delete state[key];
 
     Object.defineProperty(this, key, overriddenDescriptor);
 };
