5f2d3b2b327b7c368a127418661a8ffef4343f0f
Harold Thetiot <hthetiot@gmail.com>
2017-05-04 14:56:47 -0700

    make sure test fail in case of error

--- test/all.js
+++ test/all.js
@@ -30,4 +30,5 @@ module.exports = require("montage-testing").run(require, [
     console.log('montage-testing', 'End');
 }, function (err) {
     console.log('montage-testing', 'Fail', err, err.stack);
+    throw err;
 });
