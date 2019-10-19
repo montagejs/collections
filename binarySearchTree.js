d6fd6ef46bcfc8c8cf3eb278edda464b4b806ff9
Aditya Baranwal <baranwal.adi@gmail.com>
2019-10-19 15:52:13 +0530

    binarySearchTree code Structure

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -1,2 +1,38 @@
-Plzz enter something
-hi
+class node {
+
+    nodeVlaue = null;
+    rightNode = null;
+    leftNode = null;
+
+    constructor(value) {
+        if(value instanceof Number) {
+            this.nodeVlaue = value;
+        } else throw new Error("Value Should be Number");
+    }
+}
+
+
+class BinarySearhTree {
+    
+
+    constructor() {
+        node = new node();
+    }
+
+    insert(value) {
+        if(value instanceof Number) {
+            
+        } else throw new Error("Value Should be Number");
+
+    }
+
+    delete(value) {
+
+    }
+
+    search(value) {
+
+    }
+
+
+}
