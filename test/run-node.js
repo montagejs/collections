2389324655dc113e0e2b5a29aba3f9eee26c7a40
Benoit Marchant <marchant@mac.com>
2020-03-31 15:43:21 -0700

    add mr as dev dependency to avoid poking in node_modules location that can change between npm v2 and post v3.

--- test/run-node.js
+++ test/run-node.js
@@ -5,7 +5,7 @@ var JasmineConsoleReporter = require('jasmine-console-reporter');
 // Init
 var jasmine = jasmineRequire.core(jasmineRequire);
 var jasmineEnv = jasmine.getEnv();
-    
+
 // Export interface
 var jasmineInterface = jasmineRequire.interface(jasmine, jasmineEnv);
 global.jasmine = jasmine;
@@ -14,17 +14,17 @@ for (var property in jasmineInterface) {
     if (jasmineInterface.hasOwnProperty(property)) {
        global[property] = jasmineInterface[property];
     }
-} 
+}
 
 // Default reporter
 jasmineEnv.addReporter(jasmineInterface.jsApiReporter);
 
 // Html reporter
 var consoleReporter = new JasmineConsoleReporter({
-    colors: 1,         
-    cleanStack: 1,      
-    verbosity: 4,        
-    listStyle: 'indent', 
+    colors: 1,
+    cleanStack: 1,
+    verbosity: 4,
+    listStyle: 'indent',
     activity: false
 });
 jasmineEnv.addReporter(consoleReporter);
@@ -38,7 +38,7 @@ jasmineEnv.addReporter({
 });
 
 // Execute
-var mrRequire = require('../node_modules/montage-testing/node_modules/montage/node_modules/mr/bootstrap-node');
+var mrRequire = require('mr/bootstrap-node');
 var PATH = require("path");
 mrRequire.loadPackage(PATH.join(__dirname, ".")).then(function (mr) {
     return mr.async("all");
